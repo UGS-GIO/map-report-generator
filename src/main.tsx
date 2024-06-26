aa07842d1d4a9946890baa116b0a4e092ac68f8a
Clinton Lunn <clunn@utah.gov>
2024-06-25 23:23:46 -0600

    feat: copy pasting report generator in from legacy app

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
+import ReportAppWrapper from "./components/report/ReportAppWrapper";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    Use this to run a local development environment of the library for testing
+    <ReportAppWrapper />
   </React.StrictMode>
 );
